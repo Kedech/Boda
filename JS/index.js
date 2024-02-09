6a0662ebc42b6c523f721aff8bd21500ad456d36
Kedech <kevindelgadochavez.cj@gmail.com>
2024-02-09 13:38:25 -0500

    nuevos archivos

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,43 @@
+// Consulto tipo de dispositivo
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+  var device = 'mobile';
+} else {
+  var device = 'desktop';
+}
+//fin Consulto tipo de dispositivo
+
+// Funcion para validar formato url
+function is_url(str) {
+  regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//<!-- Soporte WEBP -->
+function support_format_webp() {
+  var elem = document.createElement('canvas');
+
+  if (!!(elem.getContext && elem.getContext('2d'))) {
+    // was able or not to get WebP representation
+    return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+  } else {
+    // very old browser like IE 8, canvas not supported
+    return false;
+  }
+}
+//Animacion loader
+let svgContainerLoader = document.querySelector('.loader');
+
+let animLoader = bodymovin.loadAnimation({
+  wrapper: svgContainerLoader,
+  animType: 'svg',
+  loop: true,
+  path: "../animations/corazon.json"//_pathProducto + "img/corazon.json"
+});
 
 //animacion de flores
     let svgContainerFlores1 = document.querySelector('.portada-flor-izq-sup');
@@ -102,18 +142,18 @@ if (device == 'mobile' || $(window).width() < 768) {
 
   // Aceptar webp?
   if (support_format_webp()) {
-    var imageParallax = '../src/photo3.jpg';
+    var imageParallax = '../src/photo2.jpg';
   } else {
-    var imageParallax = '../src/photo3.jpg';
+    var imageParallax = '../src/photo2.jpg';
   }
 
 } else {
 
   // Aceptar webp?
   if (support_format_webp()) {
-    var imageParallax = '../src/photo3.jpg';
+    var imageParallax = '../src/photo2.jpg';
   } else {
-    var imageParallax = '../src/photo3.jpg';
+    var imageParallax = '../src/photo2.jpg';
   }
 
 }
